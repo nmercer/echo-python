22ebd086ced5fb304509caea64964e5db4c99a72
nmercer <nm3rc3r@gmail.com>
2015-01-31 20:00:44 -0500

    Bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amazon-echo',
-    version='0.1',
+    version='0.2',
 
     description='Amazon Echo Python API',
     url='https://github.com/nmercer/echo-python',
